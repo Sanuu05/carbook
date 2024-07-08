d3da122a10094a5699d3df2ac0bf915dc7e3ce3e
ShantanuDesuntech23 <shantanu.desuntech@gmail.com>
2024-07-08 23:14:07 +0530

    refactor

--- src/components/pages/Searchbar.js
+++ src/components/pages/Searchbar.js
@@ -1,52 +1,65 @@
-import React from 'react'
-import axios from 'axios'
-import { port } from '../../action/user'
-function Searchbar({setalladd,setsstatus,select,alladd,sstatus,setselect}) {
-    const search1 = async (v) => {
-        try {
-            setselect('')
-            setsstatus(v)
-            const { data } = await axios.get(`${port}/main/search/${v}`)
-            setalladd(data)
-        } catch (error) {
+import React from "react";
+import axios from "axios";
+import { port } from "../../action/user";
+function Searchbar({
+  setalladd,
+  setsstatus,
+  select,
+  alladd,
+  sstatus,
+  setselect,
+}) {
+  const search1 = async (v) => {
+    try {
+      setselect("");
+      setsstatus(v);
+      // const { data } = await axios.get(`${port}/main/search/${v}`)
+      const { data } = await axios.get(
+        `https://nominatim.openstreetmap.org/search?format=json&q=${v}`
+      );
+      setalladd(data);
+    } catch (error) {}
+  };
+  const callb = (cb, t) => {
+    let timer;
+    return function (...args) {
+      if (timer) clearTimeout(timer);
+      timer = setTimeout(() => {
+        cb(...args);
+      }, t);
+    };
+  };
 
-        }
-
-
-    }
-    const callb = (cb, t) => {
-        let timer;
-        return function (...args) {
-            if (timer) clearTimeout(timer);
-            timer = setTimeout(() => {
-                cb(...args)
-            }, t)
-        }
-    }
-
-    const search = callb(search1, 1000)
+  const search = callb(search1, 1000);
   return (
     <div>
-        <div style={{ position: 'relative' }}>
-                        <input placeholder='Location' value={select?.address} type='text' className='sinput' onChange={(e) => search(e.target.value)} />
-                        {
-                            alladd && sstatus ?
-                                <div className='suggest' >
-                                    {
-                                        alladd?.map((v, i) => {
-                                            return <p onClick={() => {
-                                                setselect(v)
-                                                setsstatus('')
-                                            }}>{v?.display_name}</p>
-                                        })
-                                    }
-
-                                </div>
-                                : null
-                        }
-                    </div>
+      <div style={{ position: "relative" }}>
+        <input
+          placeholder="Location"
+          value={select?.address}
+          type="text"
+          className="sinput"
+          onChange={(e) => search(e.target.value)}
+        />
+        {alladd && sstatus ? (
+          <div className="suggest">
+            {alladd?.map((v, i) => {
+              return (
+                <p
+                  onClick={() => {
+                    setselect(v);
+                    setsstatus("");
+                  }}
+                >
+                  {v?.display_name}
+                </p>
+              );
+            })}
+          </div>
+        ) : null}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Searchbar
+export default Searchbar;
